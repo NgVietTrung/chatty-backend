9f379e6dc92513dd79c50e6d9520f04b0324cf13
Viet Trung <viettrung29122000@gmail.com>
2023-01-08 02:24:09 +0700

    feature: implemented auth feature

---
+++ src/shared/services/redis/user.cache.ts
@@ -0,0 +1,119 @@
+import { Helper } from '@global/helpers/helpers';
+import Logger from 'bunyan';
+import { config } from '@root/config';
+import { IUserDocument } from '@user/interfaces/user.interface';
+import { BaseCache } from '@service/redis/base.cache';
+import { ServerError } from '@global/helpers/error-handler';
+
+const log: Logger = config.createLogger('userCache');
+
+export class UserCache extends BaseCache {
+  constructor() {
+    super('userCache');
+  }
+
+  public async saveUserToCache(key: string, userUId: string, createdUser: IUserDocument): Promise<void> {
+    const createdAt = new Date();
+    const {
+      _id,
+      uId,
+      username,
+      email,
+      avatarColor,
+      blocked,
+      blockedBy,
+      postsCount,
+      profilePicture,
+      followersCount,
+      followingCount,
+      notifications,
+      work,
+      location,
+      school,
+      quote,
+      social,
+      bgImageVersion,
+      bgImageId
+    } = createdUser;
+
+    const firstList: string[] = [
+      '_id',
+      `${_id}`,
+      'uId',
+      `${uId}`,
+      'username',
+      `${username}`,
+      'email',
+      `${email}`,
+      'avatarColor',
+      `${avatarColor}`,
+      'postsCount',
+      `${postsCount}`,
+      'profilePicture',
+      `${profilePicture}`,
+      'blocked',
+      JSON.stringify(blocked),
+      'blockedBy',
+      JSON.stringify(blockedBy)
+    ];
+
+    const secondList: string[] = [
+      'notifications',
+      JSON.stringify(notifications),
+      'social',
+      JSON.stringify(social),
+      'followersCount',
+      `${followersCount}`,
+      'followingCount',
+      `${followingCount}`,
+      'work',
+      `${work}`,
+      'location',
+      `${location}`,
+      'school',
+      `${school}`,
+      'quote',
+      `${quote}`,
+      'bgImageVersion',
+      `${bgImageVersion}`,
+      'bgImageId',
+      `${bgImageId}`
+    ];
+
+    const dataToSave: string[] = [...firstList, ...secondList];
+
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+      await this.client.ZADD('user', { score: parseInt(userUId, 10), value: `${key}` });
+      for (let i = 0; i < dataToSave.length; i += 2) {
+        await this.client.HSET(`users:${key}`, dataToSave[i], dataToSave[i + 1]);
+      }
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try again');
+    }
+  }
+
+  public async getUserFromCache(userId: string): Promise<IUserDocument | null> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect();
+      }
+      const response: IUserDocument = (await this.client.HGETALL(`users:${userId}`)) as unknown as IUserDocument;
+      response.createdAt = new Date(Helper.parseJson(`${response.createdAt}`));
+      response.postsCount = Helper.parseJson(`${response.postsCount}`);
+      response.blocked = Helper.parseJson(`${response.blocked}`);
+      response.blockedBy = Helper.parseJson(`${response.blockedBy}`);
+      response.notifications = Helper.parseJson(`${response.notifications}`);
+      response.social = Helper.parseJson(`${response.social}`);
+      response.followersCount = Helper.parseJson(`${response.followersCount}`);
+      response.followingCount = Helper.parseJson(`${response.followingCount}`);
+      return response;
+    } catch (error) {
+      log.error(error);
+      throw new ServerError('Server error. Try again');
+    }
+  }
+}
